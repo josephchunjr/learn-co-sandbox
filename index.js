2d6f91054f9b42014c57dea5e1488ca354e1958a
joseph Chun <jjunior.chun@gmail.com>
2019-02-06 01:30:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-const data = fetch("https://api.github.com/users/mirandamon")
+
+
+
 
 //asynchronous programming. 
 // we don't want to block the UI from loading until everthing is done. we want to show things as soon as it is there.
+
+function whenDataIsLoaded(data) {
+  console.log(data)
+}
+
+function putNameOnPage (githubJson) {
+  const username = githubJson/login
+  
+}
+
+//fetch uses promises
+//1. convert data into json
+//2. take the json and do something to it
+fetch("https://api.github.com/users/mirandamon")
+.then((data) => data.json())
+.then((json) => putNameOnPage(json))
