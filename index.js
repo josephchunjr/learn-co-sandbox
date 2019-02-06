aabcc02aa1e2984204f8b552d0359a116c0480da
joseph Chun <jjunior.chun@gmail.com>
2019-02-06 01:16:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 const data = fetch("https://api.github.com/users/mirandamon")
+
+//asynchronous programming. 
+// we don't want to block the UI from loading until everthing is done. we want to show things as soon as it is there.
